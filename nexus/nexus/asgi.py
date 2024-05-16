e0b46071b87dafc393c8eef8c31c9fb76fe557ef
lotachukwu46 <lotaodi46@gmail.com>
2024-05-16 18:49:27 +0100

    'Updated'

--- nexus/nexus/asgi.py
+++ nexus/nexus/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for stash project.
+ASGI config for nexus project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stash.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nexus.settings')
 
 application = get_asgi_application()
--- nexus/nexus/urls.py
+++ nexus/nexus/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for stash project.
+URL configuration for nexus project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
