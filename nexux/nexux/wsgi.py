ce370977db2a8d34cddd04afdcb30299e25981ff
lotachukwu46 <lotaodi46@gmail.com>
2024-05-18 17:04:18 +0100

    I think i found the right name

--- nexux/nexux/wsgi.py
+++ nexux/nexux/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nexus project.
+WSGI config for nexux project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nexus.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nexux.settings')
 
 application = get_wsgi_application()
