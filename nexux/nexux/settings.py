cfc84c3359c54bd29d723db186ee98d1ef3160b7
lotachukwu46 <lotaodi46@gmail.com>
2024-05-19 18:48:15 +0100

    'Updated'

--- nexux/nexux/settings.py
+++ nexux/nexux/settings.py
@@ -37,7 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'products',
+    'orders',
+    'reviews',
+    'messaging',
+    'notifications',
+    'payments',
+    'admin',
+    'core',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,10 +85,15 @@ WSGI_APPLICATION = 'nexux.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nexuxdb',
+        'USER': 'nexuxuser',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
